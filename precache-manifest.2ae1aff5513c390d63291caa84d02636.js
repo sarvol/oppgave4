36784f0f3fe7a9b500329ae7a98184242f8d1211
sarvol <savo1009@gmail.com>
2019-12-04 16:30:39 +0100

    Updates

---
+++ precache-manifest.2ae1aff5513c390d63291caa84d02636.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cae2d237f98205cca9799f204d4baea",
+    "url": "/oppgave4/home/index.html"
+  },
+  {
+    "revision": "591fee068ae8206e9329",
+    "url": "/oppgave4/home/static/css/main.658c19fc.chunk.css"
+  },
+  {
+    "revision": "ee9794c1451b9e8f2743",
+    "url": "/oppgave4/home/static/js/2.3457efba.chunk.js"
+  },
+  {
+    "revision": "591fee068ae8206e9329",
+    "url": "/oppgave4/home/static/js/main.21f7132f.chunk.js"
+  },
+  {
+    "revision": "8f3f8c63ae810c2ddc20",
+    "url": "/oppgave4/home/static/js/runtime-main.de423420.js"
+  },
+  {
+    "revision": "0fde65db967eb68c750df139cc4748ec",
+    "url": "/oppgave4/home/static/media/logo.0fde65db.png"
+  },
+  {
+    "revision": "4f2c07ec300b6242c9c35fe1d56d16e0",
+    "url": "/oppgave4/home/static/media/selv11-12.4f2c07ec.png"
+  }
+]);
